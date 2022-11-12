ec4684fdbc6ad73ce05d1e3aaf6dc4a71ca70cb3
lenovo <lenovo@DESKTOP-J5SS8RM>
2022-11-12 14:47:00 +0700

    First Commit

---
+++ Object Repository/API Demo Graphics/elementByText.rs
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<MobileElementEntity>
+   <description></description>
+   <name>elementByText</name>
+   <tag></tag>
+   <elementGuidId>e47e16b2-579f-425f-8fd5-3d6c05650910</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <locator>//*[@text='$(element)']</locator>
+   <locatorStrategy>XPATH</locatorStrategy>
+</MobileElementEntity>
---
+++ Scripts/API Demo Graphics/Script1668237611582.groovy
@@ -0,0 +1,41 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startApplication('C:\\Users\\lenovo\\Downloads\\MOBILE APP\\ApiDemos.apk', true)
+
+Mobile.delay(5)
+
+Mobile.comment('Given User launch the app')
+
+Mobile.tap(findTestObject('Object Repository/API Demo Graphics/android.widget.Button - OK'), 0)
+
+Mobile.comment('When User taps Graphics menu')
+
+Mobile.tap(findTestObject('Object Repository/API Demo Graphics/android.widget.TextView - Graphics'), 0)
+
+Mobile.comment('Then User choose the compass')
+
+Mobile.tap(findTestObject('Object Repository/API Demo Graphics/android.widget.TextView - Compass'), 0)
+
+Mobile.pressBack()
+
+Mobile.pressBack()
+
+Mobile.closeApplication()
+
